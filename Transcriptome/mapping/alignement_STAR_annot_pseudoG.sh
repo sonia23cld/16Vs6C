5ccf4a7b2d76142bc388ccf7cd4254b12c08428f
sonia23cld <sonia.celestini@gmi.oeaw.ac.at>
2020-04-28 18:02:53 +0200

    STAR alignment updated for CBE

--- Transcriptome/mapping/alignement_STAR_annot_pseudoG.sh
+++ Transcriptome/mapping/alignement_STAR_annot_pseudoG.sh
@@ -1,56 +1,57 @@
 #!/bin/sh
 
-# PBS #
-#PBS -N STAR_align_pseudoG
-#PBS -P cegs
-#PBS -j oe
-#PBS -o Logs/STAR_align_pseudoG_^array_index^.log
-#PBS -l walltime=12:00:00,mem=10GB,ncpus=8
-#PBS -J 1-10:2
+# SLURM #
+#SBATCH --job-name=STAR_align_pseudoG
+#SBATCH --output=/users/sonia.celestini/New_data/Logs/STAR_align_pseudoG_%A_%a.log 
+#SBATCH --time=8:00:00
+#SBATCH --mem-per-cpu=10GB
+#SBATCH --cpus-per-task=8
+#SBATCH --array=1-48
 
 # MODULES #
-ml STAR/2.5.3a-foss-2016a
-ml SAMtools/1.3.1-foss-2016a
+ml star/2.7.1a-foss-2018b
+ml samtools/1.9-foss-2018b
 
 # DATA #
-i=$PBS_ARRAY_INDEX
-mainDir=${WORK}Transcriptome/6vs16/Data/
-fqLst=${mainDir}CD3TJANXX_fastqList.txt
-samples=${mainDir}samples.txt
-raw_fq1_gz=${mainDir}$(sed -n ${i}p $fqLst)
-raw_fq2_gz=${raw_fq1_gz/end1/end2}
+i=$SLURM_ARRAY_TASK_ID
+mainDir=/scratch-cbe/users/sonia.celestini/FASTQraw_trimmed/
+samples=/users/sonia.celestini/New_data/samples_updated.txt
+fqLst=$(awk '{ if ($11 == "yes") print $5 }' $samples)
+sample=$(echo $fqLst | cut -d" " -f$i)
+#raw_fq1_zip=${mainDir}${sample}.end1_val_1_fastqc.zip
+#raw_fq2_zip=${raw_fq1_zip/end1_val_1/end2_val_2}
 
-raw_fq1=${mainDir}$(basename -s .gz $raw_fq1_gz)
-raw_fq2=${mainDir}$(basename -s .gz $raw_fq2_gz)
+#raw_fq1=${mainDir}$(basename -s .zip $raw_fq1_zip)
+#raw_fq2=${mainDir}$(basename -s .zip $raw_fq2_zip)
 
-fqbase=$(basename -s .end1.fastq $raw_fq1)
-#select sample number (five digit number followed by underscore
-sample=$(echo $fqbase | grep -o -E '[0-9]{5}_')
-# remove underscore from sample number
-sample=${sample%'_'}
-accession=$(awk -v sample="$sample" '$1==sample {print $2}' $samples)
+raw_fq1=${mainDir}${sample}.end1_val_1.fq
+raw_fq2=${mainDir}${sample}.end2_val_2.fq  
+
+#get accession and short sample name                                                            
+accession=$(awk -v l="$sample" '$5==l {print $2}' $samples)
+fqbase=$(awk -v l="$sample" '$5==l {print $1}' $samples)
 
 if [ $accession = 6909 ]
 then
-	indices=$HOME/Data/TAIR10/STAR_indices_Araport11_125bpreads/
+	indices=/users/sonia.celestini/New_data/Results/pseudogenomes/TAIR10_6909_STAR_idx/
 else
-	indices=$HOME/Data/TAIR10/PseudoGenome_STAR_Indices/pseudoTAIR10_${accession}/
+	indices=/users/sonia.celestini/New_data/Results/pseudogenomes/pseudoTAIR10_${accession}_STAR_idx/
 fi
 
 echo using STAR indices from: $indices
 
 cores=8
-STAR_out=${mainDir}Alignement_STAR_annot/${fqbase}_pseudoG/
+STAR_out=/users/sonia.celestini/New_data/Results/Alignement_STAR_annot/${fqbase}_pseudoG/
 
 mkdir -p $STAR_out
 
 # unzip fastq files #
-echo 'unzipping files:'
-echo $raw_fq1_gz
-echo $raw_fq2_gz
+#echo 'unzipping files:'
+#echo $raw_fq1_zip
+#echo $raw_fq2_zip
 
-gunzip $raw_fq1_gz
-gunzip $raw_fq2_gz
+#unzip $raw_fq1_zip -d $mainDir
+#unzip $raw_fq2_zip -d $mainDir
 
 # RUN #
 STAR \
@@ -67,8 +68,11 @@ STAR \
 --readFilesIn $raw_fq1 $raw_fq2 \
 --outFileNamePrefix $STAR_out
 
+
+echo 'Job finished'
+
 # rezip fastq files #
-echo 'rezipping'
-pigz $raw_fq1
-pigz $raw_fq2
+#echo 'rezipping'
+#zip -rm ${raw_fq1}.zip $raw_fq1
+#zip -rm ${raw_fq2}.zip $raw_fq2
 
