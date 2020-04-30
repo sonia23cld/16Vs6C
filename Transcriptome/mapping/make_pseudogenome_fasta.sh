f15b838f65e48247b98b63dd36facff48b6fd23a
sonia23cld <sonia.celestini@gmi.oeaw.ac.at>
2020-04-30 16:51:51 +0200

    Modified to be used with my data

--- Transcriptome/mapping/make_pseudogenome_fasta.sh
+++ Transcriptome/mapping/make_pseudogenome_fasta.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # SLURM #
-#SBATCH --output Logs/makePseudeGenomes_%A_%a.log
+#SBATCH --output=/users/sonia.celestini/New_data/Logs/makePseudeGenomes_%A_%a.log
 #SBATCH --mem=10GB
 #SBATCH --array=1-7
 
 # MODULES #
 ml python/3.6.6-foss-2018b
 # copy script from /projects/cegs/6vs16/Scripts/make_pseudogenome_fasta.py
-PSEUDOGENIZE=~/GitRepos/16Vs6C/Transcriptome/mapping/make_pseudogenome_fasta.py 
-
+PSEUDOGENIZE=/users/sonia.celestini/New_data/16Vs6C/Transcriptome/mapping/make_pseudogenome_fasta.py 
 # DATA #
 i=$SLURM_ARRAY_TASK_ID
 mainDir=/scratch-cbe/users/pieter.clauw/16vs6/Data/Genome/
+MY_PATH=/users/sonia.celestini/New_data/Results/
 FASTA=${mainDir}TAIR10_chromosomes.fas
-VCFlst=${mainDir}vcf_for_pseudogenome.txt
-VCF=$(sed -n ${i}p $VCFlst)
+VCFlst=${MY_PATH}vcf_for_pseudogenome.txt
+VCF=${MY_PATH}$(sed -n ${i}p $VCFlst)
 OUT=${VCF/1001genomes_snp-short-indel_only_ACGTN/pseudogenomes/pseudoTAIR10}
 OUT=${OUT/.vcf/.fasta}
 
