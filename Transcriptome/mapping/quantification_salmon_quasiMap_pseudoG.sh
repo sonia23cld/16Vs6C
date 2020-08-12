f5d36e6f32db46cddd83ea3b2faf22347d6b5e50
sonia23cld <sonia.celestini@gmi.oeaw.ac.at>
2020-08-12 13:05:26 +0200

    Modified scripts used by Sonia

--- Transcriptome/mapping/quantification_salmon_quasiMap_pseudoG.sh
+++ Transcriptome/mapping/quantification_salmon_quasiMap_pseudoG.sh
@@ -2,39 +2,38 @@
 
 # SLURM #
 #SBATCH --job-name=salmonQuasi
-#SBATCH --output=/users/sonia.celestini/New_data/Logs/salmon_quasiMap_Araport11_pseudoG_^array_index^.log
-#SBATCH --time=1:00:00
+#SBATCH --output=/users/sonia.celestini/New_data/Logs/salmon_quasiMap_Araport11_pseudoG_%A_%a.log
+#SBATCH --time=2:00:00
 #SBATCH --mem-per-cpu=20GB
 #SBATCH --cpus-per-task=4
-#SBATCH --array=1-48
+#SBATCH --array=1
 
 # MODULES #
-ml Salmon/0.12.0-foss-2018b-Python-3.7.4
+ml --ignore-cache salmon/1.2.1-foss-2018b
 
 # DATA #
 i=$SLURM_ARRAY_TASK_ID
-DATA=/lustre/scratch/users/pieter.clauw/Transcriptome/6vs16/Data/
-FASTQdir=${DATA}Trimmed_illumina/
-RESULTS=/lustre/scratch/users/pieter.clauw/Transcriptome/6vs16/Results/
-SAMPLES=${DATA}samples.txt
+DATA=/scratch-cbe/users/sonia.celestini/
+myDir=/users/sonia.celestini/New_data/
+FASTQdir=${DATA}FASTQraw_trimmed/
+RESULTS=${myDir}Results/
+SAMPLES=${myDir}samples_updated.txt
 
 # Select correct index file, based on pseudo genome
-base=$(awk '$6 == "ok" {print $5}' $SAMPLES | sed -n ${i}p)
-sample=$(echo $base | grep -o -E '[0-9]{5}_')
-sample=${sample%'_'}
-acn=$(awk -v sample="$sample" '$1==sample {print $2}' $SAMPLES)
+base=$(awk '{ if ($11 == "yes") print $5 }' $SAMPLES | sed -n ${i}p)
+acn=$(awk -v l="$base" '$5==l {print $2}' $SAMPLES)
 
-INDEX=${DATA}Genome/Araport11_transcriptome_${acn}_salmonIdx
+INDEX=${DATA}SalmonIdx/Araport11_transcriptome_${acn}_salmonIdx
 
 # select fastq files of given sample
-end1=${FASTQdir}${base}.end1_val_1.fq.gz
-end2=${FASTQdir}${base}.end2_val_2.fq.gz
+end1=${FASTQdir}${base}.end1_val_1.fq
+end2=${FASTQdir}${base}.end2_val_2.fq
 
 
-outdir=${RESULTS}SalmonQuant/Araport11/${base}_quasiMap_pseudoG_Trimmed_illumina/
+outdir=${RESULTS}SalmonQuant/${base}_quasiMap_pseudoG_Trimmed/
 
 # PREP #
-mkdir -p ${RESULTS}SalmonQuant/Araport11
+mkdir -p $outdir
 
 salmon quant -i $INDEX -l ISR --seqBias --gcBias --writeUnmappedNames --validateMappings --rangeFactorizationBins 4 \
 	-p 4 \
--- Transcriptome/mapping/salmon_index_Araport11_pseudoG.sh
+++ Transcriptome/mapping/salmon_index_Araport11_pseudoG.sh
@@ -2,37 +2,69 @@
 
 # script to build the transcriptome index for the Araport11 transcriptome that can be used for gene expression quantification with salmon (non-alignement based)
 
-# PBS #
-#PBS -N salmonIdx
-#PBS -P cegs
-#PBS -j oe
-#PBS -o Logs/salmon_index_PseudoG_^array_index^.log
-#PBS -l walltime=1:00:00
-#PBS -J 0-3
+# SLURM #
+#SBATCH --job-name=salmonIdx
+#SBATCH --output=/users/sonia.celestini/New_data/Logs/salmon_index_PseudoG_%A_%a.log
+#SBATCH --time=1:00:00
+#SBATCH --array=0-7
 
 
 # MODULES #
-ml Salmon/0.12.0-foss-2018b-Python-2.7.15
+ml --ignore-cache salmon/1.2.1-foss-2018b
+ml --ignore-cache gffread/0.11.8-gcccore-8.3.0
 
 # DATA #
-i=$PBS_ARRAY_INDEX
-DATA=/lustre/scratch/users/pieter.clauw/Transcriptome/6vs16/Data/
+DATA=/scratch-cbe/users/sonia.celestini/
+myDir=/users/sonia.celestini/New_data/Results/
+i=$SLURM_ARRAY_TASK_ID
 
-accessions=('6909' '9559' '6017' '9728') 
+accessions=('6909' '9559' '6017' '9728' '8242' '9888' '9433' '9075') 
 acn=${accessions[$i]}
+
+##create pseudotranscriptome first, you need gene annotations and pseudogenome
+
+#wget  https://www.arabidopsis.org/download_files/Genes/Araport11_genome_release/Araport11_GFF3_genes_transposons.201606.gff.gz
+
+Gene_annot=${DATA}Araport11_transcriptome/Araport11_GFF3_genes_transposons.201606.gff.gz
+gunzip $Gene_annot
+Gene_annot=${DATA}Araport11_transcriptome/Araport11_GFF3_genes_transposons.201606.gff
+out=${DATA}pseudotranscriptomes/pseudoAraport11_${acn}.fa 
 
 if [ $acn = 6909 ]
 then
-        TRANSCRIPT=${DATA}Genome/TAIR10_transcriptome.fasta
-	INDEX=${DATA}Genome/Araport11_transcriptome_6909_salmonIdx
+	pseudogenome=/scratch-cbe/users/pieter.clauw/16vs6/Data/Genome/TAIR10_chromosomes.fas
 else
-        TRANSCRIPT=${DATA}Genome/pseudoTAIR10_${acn}_transcriptome.fasta
-	INDEX=${DATA}Genome/Araport11_transcriptome_${acn}_salmonIdx
+	pseudogenome=${myDir}pseudogenomes/pseudoTAIR10_${acn}.fasta
 fi
 
-echo for accession $accession we are using transcriptome from: $TRANSCRIPT
+##change name of chloroplast and mitochondria to make it work
+sed -i 's/chloroplast/ChrC/g' $pseudogenome
+sed -i 's/mitochondria/ChrM/g' $pseudogenome
 
-# build index
-salmon index -t $TRANSCRIPT -k 31 -i $INDEX --keepDuplicates
+
+echo 'Pseudotranscriptome creation'
+gffread -F -w $out -g $pseudogenome $Gene_annot 
+
+echo 'Zip annotation'
+gzip -f $Gene_annot
+
+##create decoys
+decoy=${DATA}pseudotranscriptomes/decoy_${acn}.txt
+echo 'Create decoys'
+
+grep "^>" < $pseudogenome | cut -d " " -f 1 > $decoy
+sed -i.bak -e 's/>//g' $decoy
+
+gentrome=${DATA}pseudotranscriptomes/gentrome_${acn}.fa
+cat $out $pseudogenome > $gentrome
+gzip -f $gentrome
+gentrome=${DATA}pseudotranscriptomes/gentrome_${acn}.fa.gz
+
+##create indexes
+echo 'Create indexes'
+echo for accession $accession we are using transcriptome from: $out
+INDEX=/scratch-cbe/users/sonia.celestini/SalmonIdx/Araport11_transcriptome_${acn}_salmonIdx
+
+salmon index -t $gentrome -d $decoy -p 12 -i $INDEX --gencode
 
 echo index for salmon quasi-mapping has been built and saved in $INDEX
