3b83290a1cb2409f222773fa8bea199818573bdc
sonia23cld <sonia.celestini@gmi.oeaw.ac.at>
2020-08-12 14:08:43 +0200

    Trimming in CBE. Small changes added by Sonia

--- Transcriptome/mapping/trimming.sh
+++ Transcriptome/mapping/trimming.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # SLURM #
-#SBATCH --output Logs/trim_galore_%A_%a.log
-#SBATCH --array=1-119:2
+#SBATCH --output=/users/sonia.celestini/New_data/Logs/trim_galore_%A_%a.log
+#SBATCH --array=1-95:2
 
 # MODULES #
 ml trim_galore/0.6.2-foss-2018b-python-3.6.6
@@ -10,13 +10,13 @@ ml trim_galore/0.6.2-foss-2018b-python-3.6.6
 # DATA #
 end1=$SLURM_ARRAY_TASK_ID
 end2=$(expr $end1 + 1)
-mainDir=/scratch-cbe/users/pieter.clauw/16vs6/Data/Transcriptome/
+mainDir=/scratch-cbe/users/sonia.celestini/
 OUTdir=${mainDir}FASTQraw_trimmed/
 fastqLst=${mainDir}FASTQraw_list.txt
 
 # take fastqs from fastqList.txt
-FASTQ1=$(sed "${end1}q;d" $fastqLst)
-FASTQ2=$(sed "${end2}q;d" $fastqLst)
+FASTQ1=${mainDir}FASTQraw/$(sed "${end1}q;d" $fastqLst)
+FASTQ2=${mainDir}FASTQraw/$(sed "${end2}q;d" $fastqLst)
 
 # start trim_galor
 echo 'starting trim_galore on files:'
