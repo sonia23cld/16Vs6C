208d0f6ad8a0dc92990569f89063eb13d926004e
sonia23cld <sonia.celestini@gmi.oeaw.ac.at>
2020-08-12 14:04:08 +0200

    Used by Sonia. Little change added

--- Transcriptome/mapping/stage_bamfiles.sh
+++ Transcriptome/mapping/stage_bamfiles.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/sh
+#!/usr/bin/sh                                                                                                                         
 
 # stage rawdata bamfiles from rawdata repository to scratch-cbe
 
 # SLURM #
-#SBATCH --output Logs/stage_bamfiles.log
+#SBATCH --output=/users/sonia.celestini/New_data/Logs/stage_bamfiles.log
 
-SAMPLES=/groups/nordborg/projects/cegs/16Vs6C/Data/Transcriptome/RawData/samples.txt
-BAMFILES=($(awk 'NR>1 {print $10}' $SAMPLES))
+SAMPLES=/groups/nordborg/projects/cegs/16Vs6C/Data/Transcriptome/RawData/samples_updated.txt
+BAMFILES=$(awk '{ if ($11 == "yes") print $10 }' $SAMPLES)
 
-TARGET=/scratch-cbe/users/pieter.clauw/16vs6/Data/Transcriptome/BAMraw/
+TARGET=/scratch-cbe/users/sonia.celestini/BAMraw/
 
 for BAM in ${BAMFILES[@]}; do
-    cp -v $BAM $TARGET
-done
-
+	    cp -v $BAM $TARGET
+    done
 
